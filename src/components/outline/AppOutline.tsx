2f9d8059e493b7e1d6ecddf04f10823993a0012f
Amy <45153739+amtang325@users.noreply.github.com>
2023-06-10 18:28:37 +0200

    added new files

--- src/components/outline/AppOutline.tsx
+++ src/components/outline/AppOutline.tsx
@@ -2,11 +2,13 @@ import { Footer } from "@hex-labs/core";
 import React from "react";
 
 import Navigation from "./Navigation";
+import Display from "./Display";
 
 const AppOutline: React.FC = () => {
   return (
     <>
       <Navigation />
+      <Display />
       <Footer />
     </>
   );
---
+++ src/components/outline/Display.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Display: React.FC = () => {
+  return (
+    <>
+    </>
+  );
+};
+
+export default Display;
