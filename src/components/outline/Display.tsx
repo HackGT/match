951f4258fbfb7c1475acaaa528c3d1989c3fc863
reesewang25 <reesewang10@gmail.com>
2023-06-25 22:54:32 -0400

    main page stuff added

--- src/components/outline/Display.tsx
+++ src/components/outline/Display.tsx
@@ -1,9 +1,33 @@
 import React from "react";
+import { Input, Button, Menu, Box, SimpleGrid, MenuButton } from "@chakra-ui/react";
 
 const Display: React.FC = () => {
   return (
-    <>
-    </>
+    <div>
+      <div id="filters">
+        <Input placeholder="Search" width={250} outlineColor="purple"></Input>
+        <Menu>
+          <MenuButton width={250} outlineColor={"purple"}>Skills</MenuButton>
+        </Menu>
+        <Menu>
+          <MenuButton width={250} outlineColor={"purple"}>Track</MenuButton>
+        </Menu>
+        <Menu>
+          <MenuButton width={250} outlineColor={"purple"}>Schools</MenuButton>
+        </Menu>
+      </div>
+      <h1>HackGT 9 Team Formation </h1>
+      <SimpleGrid columns={4} spacing={5}>
+        <Box bg="blue" height="100px"></Box>
+        <Box bg="blue" height="100px"></Box>
+        <Box bg="blue" height="100px"></Box>
+        <Box bg="blue" height="100px"></Box>
+        <Box bg="blue" height="100px"></Box>
+        <Box bg="blue" height="100px"></Box>
+        <Box bg="blue" height="100px"></Box>
+        <Box bg="blue" height="100px"></Box>
+      </SimpleGrid>
+    </div>
   );
 };
 
