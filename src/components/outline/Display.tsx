1562abfc77aab86a76bbe52ff2d5fa94bdbc59a8
reesewang25 <reesewang10@gmail.com>
2023-07-10 22:27:26 -0400

    installed chakra-react-select and changed multi select menus

--- src/components/outline/Display.tsx
+++ src/components/outline/Display.tsx
@@ -1,74 +1,102 @@
-import React from "react";
-import { Card, Flex, Input, MenuList, MenuItem, InputGroup, Text, Icon, Button, Menu, Box, SimpleGrid, MenuButton, CardBody, InputRightElement } from "@chakra-ui/react";
-import { ChevronDownIcon } from "@chakra-ui/icons";
+import React, {useMemo} from "react";
+import { Card, Flex, Input, Text, Box, SimpleGrid, CardBody} from "@chakra-ui/react";
+import { GroupBase, OptionBase, Select} from "chakra-react-select";
 
 const Display: React.FC = () => {
   const title = process.env.REACT_APP_EVENT_NAME;
+  const skillOptions = useMemo(
+    ()=>[
+      {
+        label: "Skill 1",
+        value: "SKILL_1",
+      },
+      {
+        label: "Skill 2",
+        value: "SKILL_2",
+      },
+    ],
+    []
+  )
+  const trackOptions = useMemo(
+    ()=>[
+      {
+        label: "Track 1",
+        value: "TRACK_1",
+      },
+      {
+        label: "Track 2",
+        value: "TRACK_2",
+      },
+    ],
+    []
+  )
+  const schoolOptions = useMemo(
+    ()=>[
+      {
+        label: "School 1",
+        value: "SCHOOL_1",
+      },
+      {
+        label: "School 2",
+        value: "SCHOOL_2",
+      },
+    ],
+    []
+  )
+  interface GroupOption extends OptionBase {
+    label: string;
+    value: string;
+  }
+
   return (
     <Card width={"96%"} height={"795px"} top={"50px"} left={"2%"} boxShadow={"0px 4px 8px 0px rgba(33, 36, 41, 0.1)"}>
       <CardBody>
         <Flex>
-          <Input placeholder="Search" width={"256px"} height={"40px"} borderRadius={"4px"} border={"1px solid rgba(123, 105, 236, 1)"}/>
-          <Menu>
-            <Box display="flex" alignItems="center">
-              <InputGroup padding={"0px 0px 0px 10px"}>
-                <Input placeholder="Skills" padding={"0px 0px 0px 10px"} width={"256px"} height={"40px"} borderRadius={"4px"} border={"1px solid rgba(123, 105, 236, 1)"}/>
-                  <InputRightElement>
-                    <MenuButton height="40px" display="flex" alignItems="center">
-                      <ChevronDownIcon />
-                    </MenuButton>
-                  </InputRightElement>
-              </InputGroup>
-            </Box>
-            <MenuList>
-              <MenuItem>Skill 1</MenuItem>
-              <MenuItem>Skill 2</MenuItem>
-            </MenuList>
-          </Menu>
-          <Menu>
-            <Box display="flex" alignItems="center">
-              <InputGroup padding={"0px 0px 0px 10px"}>
-                <Input placeholder="Track" padding={"0px 0px 0px 10px"} width={"256px"} height={"40px"} borderRadius={"4px"} border={"1px solid rgba(123, 105, 236, 1)"}/>
-                  <InputRightElement>
-                    <MenuButton height="40px" display="flex" alignItems="center">
-                      <ChevronDownIcon />
-                    </MenuButton>
-                  </InputRightElement>
-              </InputGroup>
-            </Box>
-            <MenuList>
-              <MenuItem>Track 1</MenuItem>
-              <MenuItem>Track 2</MenuItem>
-            </MenuList>
-          </Menu>
-          <Menu>
-            <Box display="flex" alignItems="center">
-              <InputGroup padding={"0px 0px 0px 10px"}>
-                <Input placeholder="Schools" padding={"0px 0px 0px 10px"} width={"256px"} height={"40px"} borderRadius={"4px"} border={"1px solid rgba(123, 105, 236, 1)"}/>
-                  <InputRightElement>
-                    <MenuButton height="40px" display="flex" alignItems="center">
-                      <ChevronDownIcon />
-                    </MenuButton>
-                  </InputRightElement>
-              </InputGroup>
-            </Box>
-            <MenuList>
-              <MenuItem>School 1</MenuItem>
-              <MenuItem>School 2</MenuItem>
-            </MenuList>
-          </Menu>
+          <Input placeholder="Search" width={"256px"} height={"40px"}/>
+          
+          <Box pl="10px" w="256px">
+          <Select<GroupOption, true, GroupBase<GroupOption>>
+            isMulti
+            options={skillOptions}
+            placeholder="Skills"
+            closeMenuOnSelect={false}
+            selectedOptionStyle="check"
+            hideSelectedOptions={false}
+            useBasicStyles
+          />
+          </Box>
+
+          <Box pl="10px" w="256px">
+          <Select<GroupOption, true, GroupBase<GroupOption>>
+            isMulti
+            options={trackOptions}
+            placeholder="Tracks"
+            closeMenuOnSelect={false}
+            selectedOptionStyle="check"
+            hideSelectedOptions={false}
+            useBasicStyles
+          />
+          </Box>
+
+          <Box pl="10px" w="256px">
+          <Select<GroupOption, true, GroupBase<GroupOption>>
+            isMulti
+            options={schoolOptions}
+            placeholder="Schools"
+            closeMenuOnSelect={false}
+            selectedOptionStyle="check"
+            hideSelectedOptions={false}
+            useBasicStyles
+          />
+          </Box>
+          
         </Flex>
         <br></br>
         <Box paddingLeft={"5%"} paddingRight={"5%"}>
           <Text fontSize={32}>{title}</Text>
           <br></br>
           <SimpleGrid columns={4} spacing={"50px"}>
-            <Box bg="blue" width={"100%"} height="150px"></Box>
-            <Box bg="blue" width={"100%"} height="150px"></Box>
-            <Box bg="blue" width={"100%"} height="150px"></Box>
-            <Box bg="blue" width={"100%"} height="150px"></Box>
-            <Box bg="blue" width={"100%"} height="150px"></Box>
-            <Box bg="blue" width={"100%"} height="150px"></Box>
+            
           </SimpleGrid>
         </Box>
       </CardBody>
