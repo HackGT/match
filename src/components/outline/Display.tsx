f9d7023f52a2dcd1114c4d872b1b620dabf524c5
reesewang25 <reesewang10@gmail.com>
2023-07-10 22:30:09 -0400

    format

--- src/components/outline/Display.tsx
+++ src/components/outline/Display.tsx
@@ -1,11 +1,19 @@
-import React, {useMemo} from "react";
-import { Card, Flex, Input, Text, Box, SimpleGrid, CardBody} from "@chakra-ui/react";
-import { GroupBase, OptionBase, Select} from "chakra-react-select";
+import React, { useMemo } from "react";
+import {
+  Card,
+  Flex,
+  Input,
+  Text,
+  Box,
+  SimpleGrid,
+  CardBody,
+} from "@chakra-ui/react";
+import { GroupBase, OptionBase, Select } from "chakra-react-select";
 
 const Display: React.FC = () => {
   const title = process.env.REACT_APP_EVENT_NAME;
   const skillOptions = useMemo(
-    ()=>[
+    () => [
       {
         label: "Skill 1",
         value: "SKILL_1",
@@ -16,9 +24,9 @@ const Display: React.FC = () => {
       },
     ],
     []
-  )
+  );
   const trackOptions = useMemo(
-    ()=>[
+    () => [
       {
         label: "Track 1",
         value: "TRACK_1",
@@ -29,9 +37,9 @@ const Display: React.FC = () => {
       },
     ],
     []
-  )
+  );
   const schoolOptions = useMemo(
-    ()=>[
+    () => [
       {
         label: "School 1",
         value: "SCHOOL_1",
@@ -42,62 +50,65 @@ const Display: React.FC = () => {
       },
     ],
     []
-  )
+  );
   interface GroupOption extends OptionBase {
     label: string;
     value: string;
   }
 
   return (
-    <Card width={"96%"} height={"795px"} top={"50px"} left={"2%"} boxShadow={"0px 4px 8px 0px rgba(33, 36, 41, 0.1)"}>
+    <Card
+      width={"96%"}
+      height={"795px"}
+      top={"50px"}
+      left={"2%"}
+      boxShadow={"0px 4px 8px 0px rgba(33, 36, 41, 0.1)"}
+    >
       <CardBody>
         <Flex>
-          <Input placeholder="Search" width={"256px"} height={"40px"}/>
-          
+          <Input placeholder="Search" width={"256px"} height={"40px"} />
+
           <Box pl="10px" w="256px">
-          <Select<GroupOption, true, GroupBase<GroupOption>>
-            isMulti
-            options={skillOptions}
-            placeholder="Skills"
-            closeMenuOnSelect={false}
-            selectedOptionStyle="check"
-            hideSelectedOptions={false}
-            useBasicStyles
-          />
+            <Select<GroupOption, true, GroupBase<GroupOption>>
+              isMulti
+              options={skillOptions}
+              placeholder="Skills"
+              closeMenuOnSelect={false}
+              selectedOptionStyle="check"
+              hideSelectedOptions={false}
+              useBasicStyles
+            />
           </Box>
 
           <Box pl="10px" w="256px">
-          <Select<GroupOption, true, GroupBase<GroupOption>>
-            isMulti
-            options={trackOptions}
-            placeholder="Tracks"
-            closeMenuOnSelect={false}
-            selectedOptionStyle="check"
-            hideSelectedOptions={false}
-            useBasicStyles
-          />
+            <Select<GroupOption, true, GroupBase<GroupOption>>
+              isMulti
+              options={trackOptions}
+              placeholder="Tracks"
+              closeMenuOnSelect={false}
+              selectedOptionStyle="check"
+              hideSelectedOptions={false}
+              useBasicStyles
+            />
           </Box>
 
           <Box pl="10px" w="256px">
-          <Select<GroupOption, true, GroupBase<GroupOption>>
-            isMulti
-            options={schoolOptions}
-            placeholder="Schools"
-            closeMenuOnSelect={false}
-            selectedOptionStyle="check"
-            hideSelectedOptions={false}
-            useBasicStyles
-          />
+            <Select<GroupOption, true, GroupBase<GroupOption>>
+              isMulti
+              options={schoolOptions}
+              placeholder="Schools"
+              closeMenuOnSelect={false}
+              selectedOptionStyle="check"
+              hideSelectedOptions={false}
+              useBasicStyles
+            />
           </Box>
-          
         </Flex>
         <br></br>
         <Box paddingLeft={"5%"} paddingRight={"5%"}>
           <Text fontSize={32}>{title}</Text>
           <br></br>
-          <SimpleGrid columns={4} spacing={"50px"}>
-            
-          </SimpleGrid>
+          <SimpleGrid columns={4} spacing={"50px"}></SimpleGrid>
         </Box>
       </CardBody>
     </Card>
