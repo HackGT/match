e6085327417720dc2c28096c9440355892921b81
Phillip Phanhthourath <phillipphanhthourath@Phillips-MacBook-Pro.local>
2023-08-03 20:48:15 -0400

    useAxios api call and pulled hexathonID from env

--- src/components/outline/AppOutline.tsx
+++ src/components/outline/AppOutline.tsx
@@ -1,27 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Footer, Service, apiUrl } from "@hex-labs/core";
+import useAxios from "axios-hooks";
 
 import Navigation from "./Navigation";
 import Display from "./Display";
-import { userData } from "../../definitions/DummyUserData";
-import axios from "axios";
 import { UserCardType } from "../../types/UserCard";
 
 const AppOutline: React.FC = () => {
-  const [users, setUsers] = useState<any[]>([]);
+  const [{ data: hexData, loading: hexLoading, error: hexError }] = useAxios(
+    apiUrl(Service.HEXATHONS, `/hexathon-users/${process.env.REACT_APP_HEXATHON_ID}/users`)
+    
+  );
 
-  useEffect(() => {
-    const getUsers = async () => {
-      const hexUrl = apiUrl(Service.HEXATHONS, "/hexathon-users/647fee51768e521dc8ef88e0/users");
-      const hexData = await axios.get(hexUrl);
-      setUsers(hexData?.data?.hexathonUsers);
-    };
-    getUsers();
-  }, []);
-
-
+  const users = hexData?.hexathonUsers || [];
+  
   // Modify userData to dynamically create an object for each user in the users array
-  const userData: UserCardType[] = users.map((user) => ({
+  const userData: UserCardType[] = users.map((
+    user: { name: string; 
+      profile: { 
+        school: string; 
+        year: string; 
+        major: string; 
+        description: string; 
+        skills: string[]; 
+        commitmentLevel: string; 
+      }; 
+    }) => 
+    ({
     name: user?.name,
     profile: {
       school: user?.profile?.school,
@@ -29,9 +34,10 @@ const AppOutline: React.FC = () => {
       major: user?.profile?.major,
       description: user?.profile?.description,
       skills: user?.profile?.skills,
-      commitmentLevel: user?.profile?.commitmentLevel, 
-    }
-  }));
+      commitmentLevel: user?.profile?.commitmentLevel,
+    },
+    })
+  );
 
   return (
     <>
