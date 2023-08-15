ae5ed2394fe2d0e22828658cb225e30b686a9ae5
Amy <45153739+amtang325@users.noreply.github.com>
2023-08-15 10:32:09 -0400

    added dummy data to index file

--- src/definitions/index.tsx
+++ src/definitions/index.tsx
@@ -1,5 +1,6 @@
 import { CommitmentLevels } from "./CommitmentLevels";
 import Schools from "./Schools";
 import Skills from "./Skills";
+import { teamData } from "./DummyTeamData";
 
-export { CommitmentLevels, Schools, Skills };
+export { CommitmentLevels, Schools, Skills, teamData };
