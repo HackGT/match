64ba499127cfa450cd65e832b52699731bd44a99
Phillip Phanhthourath <phillipphanhthourath@lawn-128-61-45-230.lawn.gatech.edu>
2023-08-20 19:22:30 -0400

    created basic card design

--- src/components/TeamCard.tsx
+++ src/components/TeamCard.tsx
@@ -14,7 +14,8 @@ import userData from "../definitions/DummyTeamData";
 import teamData from "../definitions/DummyTeamData";
 
 const TeamCard: React.FC<TeamCardType> = (props: TeamCardType) => {
-    
+    const { name, members, description } = props;
+
     return (
         <Box
           borderWidth="1px"
@@ -29,6 +30,16 @@ const TeamCard: React.FC<TeamCardType> = (props: TeamCardType) => {
           cursor="pointer"
           padding="4"
         >
+            <Flex flexDirection="column">
+                <Text fontSize="3xl" fontWeight="bold" mb="2">
+                    {name}
+                </Text>
+                <Divider borderColor="gray.300" borderWidth="2px" mb="2" />
+                <Text fontSize="sm" color="gray.500" mb="2" height="45px" isTruncated>
+                    {description}
+                </Text>
+                <Divider borderColor="gray.300" borderWidth="2px" />
+            </Flex>
         </Box>
       );
 };
