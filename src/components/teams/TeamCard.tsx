530d6ef0e6d63d19353bf09a58ea51b09b03ebb7
Amy <45153739+amtang325@users.noreply.github.com>
2023-09-03 23:11:46 -0400

    added loading state

--- src/components/teams/TeamCard.tsx
+++ src/components/teams/TeamCard.tsx
@@ -20,6 +20,7 @@ const TeamCard: React.FC<TeamCardType> = (props: TeamCardType) => {
   const { name, members, description } = props;
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [ memberData, setMemberData ] = useState<any>([]);
+  const [ loaded, setLoaded ] = useState<boolean>(false);
 
 
   async function getUsers() {
@@ -34,6 +35,7 @@ const TeamCard: React.FC<TeamCardType> = (props: TeamCardType) => {
   
     const responses = await Promise.all(promises);
     setMemberData(responses);
+    setLoaded(true);
   }
 
   React.useEffect(() => {
@@ -41,66 +43,68 @@ const TeamCard: React.FC<TeamCardType> = (props: TeamCardType) => {
   }, []);
 
   return (
-    <Box
-      borderWidth="1px"
-      borderStyle="solid"
-      rounded="lg"
-      boxShadow="lg"
-      height="315px"
-      width="300px"
-      fontWeight="bold"
-      alignItems="center"
-      backgroundColor="white"
-      cursor="pointer"
-      padding="4"
-      onClick={onOpen} // Open the modal when clicked
-    >
-      <Flex flexDirection="column">
-        <Text fontSize="3xl" fontWeight="bold" mb="1">
-          {name}
-        </Text>
-        <Divider borderColor="gray.300" borderWidth="2px" mb="2" />
+    <>
+      { loaded && <Box
+        borderWidth="1px"
+        borderStyle="solid"
+        rounded="lg"
+        boxShadow="lg"
+        height="315px"
+        width="300px"
+        fontWeight="bold"
+        alignItems="center"
+        backgroundColor="white"
+        cursor="pointer"
+        padding="4"
+        onClick={onOpen} // Open the modal when clicked
+      >
+        <Flex flexDirection="column">
+          <Text fontSize="3xl" fontWeight="bold" mb="1">
+            {name}
+          </Text>
+          <Divider borderColor="gray.300" borderWidth="2px" mb="2" />
 
-        <Flex alignItems="flex-start" flexWrap="wrap" mb="2">
-          {memberData.map((member: any) => (
-            <Tag
-              key={member?.userId}
-              bg="blue.400"
-              color="white"
-              borderRadius="md"
-              px="2"
-              py="1"
-              mr="2"
-              mb="2"
-            >
-              <Text fontSize="sm">{member?.name}</Text>
+          <Flex alignItems="flex-start" flexWrap="wrap" mb="2">
+            {memberData.map((member: any) => (
+              <Tag
+                key={member?.userId}
+                bg="blue.400"
+                color="white"
+                borderRadius="md"
+                px="2"
+                py="1"
+                mr="2"
+                mb="2"
+              >
+                <Text fontSize="sm">{member?.name}</Text>
+              </Tag>
+            ))}
+
+            {memberData.map((member: any) => {
+              <h1>test{member ? member.name : ""}</h1>
+            })}
+          </Flex>
+
+          <Box
+            fontSize="sm"
+            color="gray.500"
+            mb="2"
+            maxHeight="80px" 
+            overflowY="auto" 
+          >
+            {description}
+          </Box>
+
+          <Divider borderColor="gray.300" borderWidth="2px" />
+
+          <Flex justifyContent="flex-start" alignItems="center" mt="2">
+            <Tag bg="purple.400" color="white" borderRadius="md" px="2" py="1">
+              Members: {members.length}
             </Tag>
-          ))}
-
-          {memberData.map((member: any) => {
-            <h1>test{member ? member.name : ""}</h1>
-          })}
+          </Flex>
         </Flex>
-
-        <Box
-          fontSize="sm"
-          color="gray.500"
-          mb="2"
-          maxHeight="80px" 
-          overflowY="auto" 
-        >
-          {description}
-        </Box>
-
-        <Divider borderColor="gray.300" borderWidth="2px" />
-
-        <Flex justifyContent="flex-start" alignItems="center" mt="2">
-          <Tag bg="purple.400" color="white" borderRadius="md" px="2" py="1">
-            Members: {members.length}
-          </Tag>
-        </Flex>
-      </Flex>
-    </Box>
+      </Box>}
+    </>
   );
 };
 
