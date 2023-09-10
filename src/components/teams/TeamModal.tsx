286557c7bbb44295f8580bcf278e938aa4005029
Reese Wang <reesewang2022@gmail.com>
2023-09-10 09:47:33 -0400

    switched to usercard

--- src/components/teams/TeamModal.tsx
+++ src/components/teams/TeamModal.tsx
@@ -10,16 +10,10 @@ import {
   ModalFooter,
   Button,
   Flex,
-  Box,
-  Tag
+  Box
 } from "@chakra-ui/react";
-import Avatars from "../../definitions/Avatars";
-import { commitmentLevelColors } from "../../definitions/CommitmentLevels";
-import React, { useState } from "react";
-import axios from "axios";
-import { apiUrl, Service, handleAxiosError, useAuth } from "@hex-labs/core";
+import { useAuth } from "@hex-labs/core";
 import UserCard from "../users/UserCard";
-import TeamUserCard from "./TeamUserCard";
 import { UserCardType } from "../../types/UserCard";
 export default function TeamModal(props: any) {
   const { isOpen, onClose, name, memberData, description } = props;
@@ -40,7 +34,7 @@ export default function TeamModal(props: any) {
                 <Flex flexWrap="wrap" justifyContent="space-evenly">
                 {memberData
                     .filter((hUser: any) => hUser.userId !== user?.uid)
-                    .map((user: UserCardType) => <TeamUserCard key={user.name} {...user} />)}
+                    .map((user: UserCardType) => <UserCard key={user.name} {...user} />)}
                 </Flex>
             </Box>
           </ModalBody>
