1604a7d71576ac48abcac506b68e977b817b8691
Reese Wang <reesewang2022@gmail.com>
2023-09-09 23:15:39 -0400

    made cards unmodalable

--- src/components/teams/TeamModal.tsx
+++ src/components/teams/TeamModal.tsx
@@ -10,6 +10,8 @@ import {
   ModalFooter,
   Button,
   Flex,
+  Box,
+  Tag
 } from "@chakra-ui/react";
 import Avatars from "../../definitions/Avatars";
 import { commitmentLevelColors } from "../../definitions/CommitmentLevels";
@@ -17,6 +19,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { apiUrl, Service, handleAxiosError, useAuth } from "@hex-labs/core";
 import UserCard from "../users/UserCard";
+import TeamUserCard from "./TeamUserCard";
 import { UserCardType } from "../../types/UserCard";
 export default function TeamModal(props: any) {
   const { isOpen, onClose, name, memberData, description } = props;
@@ -24,7 +27,7 @@ export default function TeamModal(props: any) {
 
   return (
     <>
-      <Modal size="xl" isOpen={isOpen} onClose={onClose}>
+      <Modal size="6xl" isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>{name}</ModalHeader>
@@ -33,12 +36,16 @@ export default function TeamModal(props: any) {
             <Divider borderColor="gray.300" borderWidth="2px" mb="2" />
             <Text>{description}</Text>
             <Divider borderColor="gray.300" borderWidth="2px" mb="2" />
-            <Flex flexWrap="wrap" justifyContent="space-evenly">
-              {memberData.map((member: any) => console.log(member.name))}
-            </Flex>
+            <Box>
+                <Flex flexWrap="wrap" justifyContent="space-evenly">
+                {memberData
+                    .filter((hUser: any) => hUser.userId !== user?.uid)
+                    .map((user: UserCardType) => <TeamUserCard key={user.name} {...user} />)}
+                </Flex>
+            </Box>
           </ModalBody>
           <ModalFooter>
-            <Button>Ask to Team Up</Button>
+            <Button backgroundColor="#7B69EC" color="white" width={"100%"}>Ask to Team Up</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
