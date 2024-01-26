e75b6cc4d2f88c15a1c28c62d09010c76af9270c
Amy <45153739+amtang325@users.noreply.github.com>
2024-01-26 14:41:01 -0500

    fixed spacing

--- src/components/UserGuide.tsx
+++ src/components/UserGuide.tsx
@@ -23,21 +23,25 @@ export default function UserGuide(props: any) {
           <ModalCloseButton />
           <ModalBody>
             <Heading size="lg" color="#7B69EC">Managing Your Profile</Heading>
+            <br />
             <Heading size = "md">1. Click on "Edit Profile" in the navigation bar.</Heading>
             <Heading size = "md">2. Click the checkbox at the bottom of the page to opt-in or opt-out of having your profile displayed in the Match portal to other participants.</Heading>
             <Heading size = "md">3. If choosing to opt-in, you can edit skills, description, and commitment level on this page.</Heading>
             <br />
             <Heading size="lg" color="#7B69EC">Requesting to Join a Team</Heading>
+            <br />
             <Heading size = "md">1. Click on the "Teams" tab.</Heading>
             <Heading size = "md">2. Click on a specific team's card to open a pop-up with more detailed information about its members.</Heading>
             <Heading size = "md">3. If you want to join, click "Ask to Team Up". Members of the team can then accept your request through their team settings box.</Heading>
             <br />
             <Heading size="lg" color="#7B69EC">Creating a New Team</Heading>
+            <br />
             <Heading size = "md">1. In order to request other people to join your team, you must first create a team.</Heading>
             <Heading size = "md">2. Click on the "Teams" tab and create a team through the team settings box at the top.</Heading>
             <Heading size = "md">3. After creating a new team, you can add known members by their emails.</Heading>
             <br />
             <Heading size="lg" color="#7B69EC">Requesting a Teammate</Heading>
+            <br />
             <Heading size = "md">1. Make sure that you have created a new team or are part of a team already.</Heading>
             <Heading size = "md">2. Click on the "Individuals" tab.</Heading>
             <Heading size = "md">3. Find potential teammates by filtering by skills and commitment level.</Heading>
@@ -46,6 +50,7 @@ export default function UserGuide(props: any) {
             <Heading size = "md">6. The participant will receive an email notification with your request and will need to go through the process of requesting to join a team.</Heading>
             <br />
             <Heading size="lg" color="#7B69EC">Managing a Team</Heading>
+            <br />
             <Heading size = "md">1. With the team settings box at the top of the "Teams" tab, you can accept member requests, add members, change descriptions, or leave the team.</Heading>
             <Heading size = "md">2. To see and accept member requests, click on the bell at the top right. A ringing bell icon means that your team has pending requests.</Heading>
             <Heading size = "md">3. To leave the team, click on the red arrow at the top right.</Heading>
