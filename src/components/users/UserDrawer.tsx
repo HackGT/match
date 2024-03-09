a39bc6e2bae73fa2b80720732e751b43c1743416
Roshan Desai <rohod04@gmail.com>
2024-03-09 10:35:36 -0500

    fix breaking changes - added null checks and made sure to wait on all data to load before rendering

--- src/components/users/UserDrawer.tsx
+++ src/components/users/UserDrawer.tsx
@@ -13,90 +13,84 @@ import {
   useToast,
 } from "@chakra-ui/react";
 import TeamCard from "../teams/TeamCard";
-import { TeamCardType } from "../../types/TeamCard"
+import { TeamCardType } from "../../types/TeamCard";
 import { AiOutlineMessage } from "react-icons/ai";
 import axios from "axios";
 import { Service, apiUrl, handleAxiosError } from "@hex-labs/core";
 
+const UserDrawer = (props: any) => {
+  const { isOpen, onOpen, onClose, teamRequests, user } = props;
+  const cancelRef = useRef<HTMLButtonElement>(null);
+  const toast = useToast();
+  const hexathon = process.env.REACT_APP_HEXATHON_ID;
 
-const UserDrawer = (props : any) => {
-    const { isOpen, onOpen, onClose, teamRequests, user } = props;
-    const cancelRef = useRef<HTMLButtonElement>(null);
-    const toast = useToast();
-    const hexathon = process.env.REACT_APP_HEXATHON_ID;
+  var invited = false;
 
-    var invited = false
+  const handleAcceptTeam = async (teamName: String) => {
+    try {
+      await axios.post(apiUrl(Service.HEXATHONS, `/teams/accept-invite`), {
+        name: teamName,
+        hexathon,
+      });
+      toast({
+        title: "Success",
+        description: `You have joined ${teamName}`,
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+    } catch (e: any) {
+      handleAxiosError(e);
+    }
+  };
 
-    const handleAcceptTeam = async(teamName : String) => {
-      try {
-        await axios.post(apiUrl(Service.HEXATHONS, `/teams/accept-invite`), {
-          name: teamName,
-          hexathon,
-        });
-        toast({
-          title: "Success",
-          description: `You have joined ${teamName}`,
-          status: "success",
-          duration: 3000,
-          isClosable: true,
-        });
-      } catch (e: any) {
-        handleAxiosError(e);
-      }
-    };
-
-    const handleDeclineTeam = async(teamName : String) => {
-      try {
-        await axios.post(apiUrl(Service.HEXATHONS, `/teams/reject-invite`), {
-          name: teamName,
-          hexathon,
-        });
-        toast({
-          title: "Success",
-          description: `You have declined the invite to join ${teamName}`,
-          status: "success",
-          duration: 3000,
-          isClosable: true,
-        });
-      } catch (e: any) {
-        handleAxiosError(e);
-      }
-    };
-    console.log('user id: ', user.userId)
-    // .filter(invite.member.userId === user.userId)
+  const handleDeclineTeam = async (teamName: String) => {
+    try {
+      await axios.post(apiUrl(Service.HEXATHONS, `/teams/reject-invite`), {
+        name: teamName,
+        hexathon,
+      });
+      toast({
+        title: "Success",
+        description: `You have declined the invite to join ${teamName}`,
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+    } catch (e: any) {
+      handleAxiosError(e);
+    }
+  };
 
   return (
     <>
-        <Drawer
-        isOpen={isOpen}
-        placement="right"
-        onClose={onClose}
-        finalFocusRef={cancelRef}
-      >
+      <Drawer isOpen={isOpen} placement="right" onClose={onClose} finalFocusRef={cancelRef}>
         <DrawerOverlay />
         <DrawerContent>
           <DrawerCloseButton />
           <DrawerHeader>Team Notifications</DrawerHeader>
           <DrawerBody>
-            {teamRequests.length == 0 ? (
+            {teamRequests?.length == 0 ? (
               <Text>No notifications</Text>
             ) : (
               <VStack align="left">
                 <Text as="b" fontSize="18px" fontWeight="bold" color="#7B69EC">
                   Team Requests
                 </Text>
-                {teamRequests.map((teamRequest: TeamCardType) => (
+                {teamRequests?.map((teamRequest: TeamCardType) => (
                   <VStack mt={2}>
                     <TeamCard key={teamRequest.name} {...teamRequest} />
                     <HStack>
                       <AiOutlineMessage />
                       {teamRequest?.sentInvites?.map((invite: any) => {
-                          if (!invited && user.uid === invite.member.userId) {
-                            invited = true
-                            return <Text fontSize="14px" color="black">
+                        if (!invited && user.uid === invite.member.userId) {
+                          invited = true;
+                          return (
+                            <Text fontSize="14px" color="black">
                               {invite.message}
                             </Text>
-                          }   
+                          );
+                        }
                       })}
                     </HStack>
                     <HStack mt={2} justify="space-evenly">
@@ -127,7 +121,7 @@ const UserDrawer = (props : any) => {
         </DrawerContent>
       </Drawer>
     </>
-  )
-}
+  );
+};
 
-export default UserDrawer
+export default UserDrawer;
